d5139c175b6b2254ff65c14ee59a37750d3bcd34
Wojciech Decker <wdk@touk.pl>
2021-06-16 16:23:57 +0200

    Support Scala.js 1.5.0

--- scripts/test-cli.sh
+++ scripts/test-cli.sh
@@ -48,11 +48,24 @@ test -s out || fail "scalajsp bin/Foo$.sjsir: empty output"
 scalajsp bin/Foo\$A.sjsir > out
 test -s out || fail "scalajsp bin/Foo\$A.sjsir: empty output"
 
-scalajsld -s -o test.js -mm Foo.main bin
+scalajsld -s -o test.js -mm Foo.main bin 2> test_stderr.txt
+grep -Fxq "Warning: using a single file as output (--output) is deprecated since Scala.js 1.3.0. Use --outputDir instead." test_stderr.txt \
+  || fail "expected warning. Got: $(cat test_stderr.txt)"
 test -s test.js || fail "scalajsld: empty output"
 test -s test.js.map || fail "scalajsld: empty source map"
 
-node test.js > got.run
+node test.js > got-legacy.run
+cat > want-legacy.run <<EOF
+asdf 2
+EOF
+
+diff got-legacy.run want-legacy.run
+
+mkdir test-output
+scalajsld -s --outputDir test-output --moduleSplitStyle SmallestModules --moduleKind CommonJSModule -mm Foo.main bin
+test "$(ls test-output/*.js| wc -w)" -gt "1" || fail "scalajsld: produced single js output file"
+
+node test-output/main.js > got.run
 cat > want.run <<EOF
 asdf 2
 EOF
